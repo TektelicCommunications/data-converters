09cfd44746c4649ee0138c5289cc55a72a5bbe53
Zach <zbrown@tektelic.com>
2025-11-18 09:35:48 -0700

    Added ebeat v1.0 data converters

---
+++ Tektelic v1/ebeat/ebeat-v1.0-decoder.js
@@ -0,0 +1,572 @@
+	var decoded_data = {};
+	var decoder = [];
+	var errors = [];
+	var bytes = convertToUint8Array(bytes);
+	decoded_data['raw'] = toHexString(bytes).toUpperCase();
+	decoded_data['port'] = port;
+	var input = {
+		"fPort": port,
+	}
+	if(input.fPort === 101){
+		decoder = [
+			{
+				key: [],
+				fn: function(arg) { 
+					var size = arg.length;
+					var invalid_registers = [];
+					var responses = [];
+					while(arg.length > 0){
+						var downlink_fcnt = arg[0];
+						var num_invalid_writes = arg[1];
+						arg = arg.slice(2);
+						if(num_invalid_writes > 0) {
+							for(var i = 0; i < num_invalid_writes; i++){
+								invalid_registers.push("0x" + arg[i].toString(16));
+							}
+							arg = arg.slice(num_invalid_writes);
+							responses.push(num_invalid_writes + ' Invalid write command(s) from DL:' + downlink_fcnt + ' for register(s): ' + invalid_registers);
+						}
+						else {
+							responses.push('All write commands from DL:' + downlink_fcnt + 'were successfull');
+						}
+						invalid_registers = [];
+					}
+					decoded_data["response"] = responses;
+					return size;
+				}
+			}
+		];
+	}
+	
+if (input.fPort === 25) {
+	decoder = [
+		{
+			key: [],
+			fn: function(arg) { 
+				decoded_data['remaining_battery_capacity'] = decode_field(arg, 1, 7, 0, "unsigned");
+				return 1;
+			}
+		},
+	];
+}
+if (input.fPort === 10) {
+	decoder = [
+		{
+			key: [],
+			fn: function(arg) { 
+				decoded_data['pulse_rate'] = decode_field(arg, 1, 7, 0, "unsigned");
+				var val = decode_field(arg, 1, 7, 7, "unsigned");
+				{switch (val){
+					case 0:
+						decoded_data['er_mode'] = "Inactive";
+						break;
+					case 1:
+						decoded_data['er_mode'] = "Active";
+						break;
+					default:
+						decoded_data['er_mode'] = "Invalid";
+				}}
+				decoded_data['SpO2'] = decode_field(arg, 1, 6, 0, "unsigned");
+				decoded_data['heart_rate_variability'] = decode_field(arg, 2, 15, 0, "unsigned");
+				decoded_data['skin_temperature'] = decode_field(arg, 1, 7, 0, "unsigned");
+				decoded_data['respiration_rate'] = decode_field(arg, 1, 7, 0, "unsigned");
+				decoded_data['signal_quality_indicator'] = decode_field(arg, 1, 7, 0, "unsigned");
+				var val = decode_field(arg, 1, 0, 0, "unsigned");
+				{switch (val){
+					case 0:
+						decoded_data['assistance_required'] = "Not Requested";
+						break;
+					case 1:
+						decoded_data['assistance_required'] = "Requested";
+						break;
+					default:
+						decoded_data['assistance_required'] = "Invalid";
+				}}
+				return 1;
+			}
+		},
+		{
+			key: [0x00],
+			fn: function(arg) { 
+				var val = decode_field(arg, 1, 7, 0, "unsigned");
+				{switch (val){
+					case 0:
+						decoded_data['report_mode'] = "Normal Reporting Mode";
+						break;
+					case 255:
+						decoded_data['report_mode'] = "Enhanced Reporting Mode";
+						break;
+					default:
+						decoded_data['report_mode'] = "Invalid";
+				}}
+				return 1;
+			}
+		},
+	];
+}
+if (input.fPort === 100) {
+	decoder = [
+		{
+			key: [0x10],
+			fn: function(arg) { 
+				var val = decode_field(arg, 2, 14, 14, "unsigned");
+				{switch (val){
+					case 0:
+						decoded_data['loramac_join_mode'] = "ABP";
+						break;
+					case 1:
+						decoded_data['loramac_join_mode'] = "OTAA";
+						break;
+					default:
+						decoded_data['loramac_join_mode'] = "Invalid";
+				}}
+				return 2;
+			}
+		},
+		{
+			key: [0x11],
+			fn: function(arg) { 
+				if(!decoded_data.hasOwnProperty('loramac_opts')) {
+					decoded_data['loramac_opts'] = {};
+				}
+				var val = decode_field(arg, 2, 0, 0, "unsigned");
+				{switch (val){
+					case 0:
+						decoded_data['loramac_opts']['confirm_mode'] = "Unconfirmed";
+						break;
+					case 1:
+						decoded_data['loramac_opts']['confirm_mode'] = "Confirmed";
+						break;
+					default:
+						decoded_data['loramac_opts']['confirm_mode'] = "Invalid";
+				}}
+				var val = decode_field(arg, 2, 1, 1, "unsigned");
+				{switch (val){
+					case 0:
+						decoded_data['loramac_opts']['sync_word'] = "Private";
+						break;
+					case 1:
+						decoded_data['loramac_opts']['sync_word'] = "Public";
+						break;
+					default:
+						decoded_data['loramac_opts']['sync_word'] = "Invalid";
+				}}
+				var val = decode_field(arg, 2, 2, 2, "unsigned");
+				{switch (val){
+					case 0:
+						decoded_data['loramac_opts']['duty_cycle'] = "Disabled";
+						break;
+					case 1:
+						decoded_data['loramac_opts']['duty_cycle'] = "Enabled";
+						break;
+					default:
+						decoded_data['loramac_opts']['duty_cycle'] = "Invalid";
+				}}
+				var val = decode_field(arg, 2, 3, 3, "unsigned");
+				{switch (val){
+					case 0:
+						decoded_data['loramac_opts']['adr'] = "Disabled";
+						break;
+					case 1:
+						decoded_data['loramac_opts']['adr'] = "Enabled";
+						break;
+					default:
+						decoded_data['loramac_opts']['adr'] = "Invalid";
+				}}
+				return 2;
+			}
+		},
+		{
+			key: [0x12],
+			fn: function(arg) { 
+				if(!decoded_data.hasOwnProperty('loramac_dr_tx')) {
+					decoded_data['loramac_dr_tx'] = {};
+				}
+				decoded_data['loramac_dr_tx']['dr_number'] = decode_field(arg, 2, 11, 8, "unsigned");
+				decoded_data['loramac_dr_tx']['tx_power_number'] = decode_field(arg, 2, 3, 0, "unsigned");
+				return 2;
+			}
+		},
+		{
+			key: [0x13],
+			fn: function(arg) { 
+				if(!decoded_data.hasOwnProperty('loramac_rx2')) {
+					decoded_data['loramac_rx2'] = {};
+				}
+				decoded_data['loramac_rx2']['frequency'] = decode_field(arg, 5, 39, 8, "unsigned");
+				decoded_data['loramac_rx2']['rx2_dr_number'] = decode_field(arg, 5, 7, 0, "unsigned");
+				return 5;
+			}
+		},
+		{
+			key: [0x20],
+			fn: function(arg) { 
+				decoded_data['norm_report_enabled'] = decode_field(arg, 1, 7, 0, "unsigned");
+				return 1;
+			}
+		},
+		{
+			key: [0x21],
+			fn: function(arg) { 
+				decoded_data['deep_sleep_report_period'] = decode_field(arg, 1, 7, 0, "unsigned");
+				return 1;
+			}
+		},
+		{
+			key: [0x61],
+			fn: function(arg) { 
+				decoded_data['failed_calibration_warning_count'] = decode_field(arg, 1, 7, 0, "unsigned");
+				return 1;
+			}
+		},
+		{
+			key: [0x34],
+			fn: function(arg) { 
+				if(!decoded_data.hasOwnProperty('pr_limits')) {
+					decoded_data['pr_limits'] = {};
+				}
+				decoded_data['pr_limits']['high_pr'] = decode_field(arg, 2, 15, 8, "unsigned");
+				decoded_data['pr_limits']['low_pr'] = decode_field(arg, 2, 7, 0, "unsigned");
+				return 2;
+			}
+		},
+		{
+			key: [0x35],
+			fn: function(arg) { 
+				if(!decoded_data.hasOwnProperty('pr_limits_ER')) {
+					decoded_data['pr_limits_ER'] = {};
+				}
+				decoded_data['pr_limits_ER']['high_pr_ER'] = decode_field(arg, 2, 15, 8, "unsigned");
+				decoded_data['pr_limits_ER']['low_pr_ER'] = decode_field(arg, 2, 7, 0, "unsigned");
+				return 2;
+			}
+		},
+		{
+			key: [0x36],
+			fn: function(arg) { 
+				decoded_data['pr_win_length'] = decode_field(arg, 1, 7, 0, "unsigned");
+				return 1;
+			}
+		},
+		{
+			key: [0x37],
+			fn: function(arg) { 
+				var val = decode_field(arg, 1, 7, 0, "unsigned");
+				{switch (val){
+					case 0:
+						decoded_data['pr_stat_function'] = "Median";
+						break;
+					case 1:
+						decoded_data['pr_stat_function'] = "Minimum";
+						break;
+					case 2:
+						decoded_data['pr_stat_function'] = "Maximum";
+						break;
+					case 3:
+						decoded_data['pr_stat_function'] = "Average";
+						break;
+					default:
+						decoded_data['pr_stat_function'] = "Invalid";
+				}}
+				return 1;
+			}
+		},
+		{
+			key: [0x40],
+			fn: function(arg) { 
+				if(!decoded_data.hasOwnProperty('SpO2_limits')) {
+					decoded_data['SpO2_limits'] = {};
+				}
+				decoded_data['SpO2_limits']['high_SpO2'] = decode_field(arg, 2, 15, 8, "unsigned");
+				decoded_data['SpO2_limits']['low_SpO2'] = decode_field(arg, 2, 7, 0, "unsigned");
+				return 2;
+			}
+		},
+		{
+			key: [0x42],
+			fn: function(arg) { 
+				decoded_data['SpO2_win_length'] = decode_field(arg, 1, 7, 0, "unsigned");
+				return 1;
+			}
+		},
+		{
+			key: [0x43],
+			fn: function(arg) { 
+				var val = decode_field(arg, 1, 7, 0, "unsigned");
+				{switch (val){
+					case 0:
+						decoded_data['SpO2_stat_function'] = "Median";
+						break;
+					case 1:
+						decoded_data['SpO2_stat_function'] = "Minimum";
+						break;
+					case 2:
+						decoded_data['SpO2_stat_function'] = "Maximum";
+						break;
+					case 3:
+						decoded_data['SpO2_stat_function'] = "Average";
+						break;
+					default:
+						decoded_data['SpO2_stat_function'] = "Invalid";
+				}}
+				return 1;
+			}
+		},
+		{
+			key: [0x51],
+			fn: function(arg) { 
+				decoded_data['ST_win_len'] = decode_field(arg, 1, 7, 0, "unsigned");
+				return 1;
+			}
+		},
+		{
+			key: [0x52],
+			fn: function(arg) { 
+				var val = decode_field(arg, 1, 7, 0, "unsigned");
+				{switch (val){
+					case 0:
+						decoded_data['ST_stat_function'] = "Median";
+						break;
+					case 1:
+						decoded_data['ST_stat_function'] = "Minimum";
+						break;
+					case 2:
+						decoded_data['ST_stat_function'] = "Maximum";
+						break;
+					case 3:
+						decoded_data['ST_stat_function'] = "Average";
+						break;
+					default:
+						decoded_data['ST_stat_function'] = "Invalid";
+				}}
+				return 1;
+			}
+		},
+		{
+			key: [0x54],
+			fn: function(arg) { 
+				if(!decoded_data.hasOwnProperty('rr_limits')) {
+					decoded_data['rr_limits'] = {};
+				}
+				decoded_data['rr_limits']['high_rr'] = decode_field(arg, 2, 15, 8, "unsigned");
+				decoded_data['rr_limits']['low_rr'] = decode_field(arg, 2, 7, 0, "unsigned");
+				return 2;
+			}
+		},
+		{
+			key: [0x55],
+			fn: function(arg) { 
+				if(!decoded_data.hasOwnProperty('rr_limits_ER')) {
+					decoded_data['rr_limits_ER'] = {};
+				}
+				decoded_data['rr_limits_ER']['high_rr_ER'] = decode_field(arg, 2, 15, 8, "unsigned");
+				decoded_data['rr_limits_ER']['low_rr_ER'] = decode_field(arg, 2, 7, 0, "unsigned");
+				return 2;
+			}
+		},
+		{
+			key: [0x56],
+			fn: function(arg) { 
+				decoded_data['rr_win_length'] = decode_field(arg, 1, 7, 0, "unsigned");
+				return 1;
+			}
+		},
+		{
+			key: [0x57],
+			fn: function(arg) { 
+				var val = decode_field(arg, 1, 7, 0, "unsigned");
+				{switch (val){
+					case 0:
+						decoded_data['rr_stat_function'] = "Median";
+						break;
+					case 1:
+						decoded_data['rr_stat_function'] = "Minimum";
+						break;
+					case 2:
+						decoded_data['rr_stat_function'] = "Maximum";
+						break;
+					case 3:
+						decoded_data['rr_stat_function'] = "Average";
+						break;
+					default:
+						decoded_data['rr_stat_function'] = "Invalid";
+				}}
+				return 1;
+			}
+		},
+		{
+			key: [0x71],
+			fn: function(arg) { 
+				if(!decoded_data.hasOwnProperty('metadata')) {
+					decoded_data['metadata'] = {};
+				}
+				decoded_data['metadata']['app_ver_major'] = decode_field(arg, 7, 55, 48, "unsigned");
+				decoded_data['metadata']['app_ver_minor'] = decode_field(arg, 7, 47, 40, "unsigned");
+				decoded_data['metadata']['app_ver_revision'] = decode_field(arg, 7, 39, 32, "unsigned");
+				decoded_data['metadata']['loramac_ver_major'] = decode_field(arg, 7, 31, 24, "unsigned");
+				decoded_data['metadata']['loramac_ver_minor'] = decode_field(arg, 7, 23, 16, "unsigned");
+				decoded_data['metadata']['loramac_ver_revision'] = decode_field(arg, 7, 15, 8, "unsigned");
+				decoded_data['metadata']['loramac_region'] = decode_field(arg, 7, 7, 0, "unsigned");
+				return 7;
+			}
+		},
+	];
+}
+
+	try {
+		for (var bytes_left = bytes.length; bytes_left > 0;) {
+			var found = false;
+			for (var i = 0; i < decoder.length; i++) {
+				var item = decoder[i];
+				var key = item.key;
+				var keylen = key.length;
+				var header = slice(bytes, 0, keylen);
+				if (is_equal(header, key)) { // Header in the data matches to what we expect
+					var f = item.fn;
+					var consumed = f(slice(bytes, keylen, bytes.length)) + keylen;
+					bytes_left -= consumed;
+					bytes = slice(bytes, consumed, bytes.length);
+					found = true;
+					break;
+				}
+			}
+			if (!found) {
+				errors.push("Unable to decode header " + toHexString(header).toUpperCase());
+				break;
+			}
+		}
+	} catch (error) {
+		errors = "Fatal decoder error";
+	}
+
+	function slice(a, f, t) {
+		var res = [];
+		for (var i = 0; i < t - f; i++) {
+			res[i] = a[f + i];
+		}
+		return res;
+	}
+
+	// Extracts bits from a byte array
+	function extract_bytes(chunk, startBit, endBit) {
+		var array = new Array(0);
+		var totalBits = startBit - endBit + 1;
+		var totalBytes = Math.ceil(totalBits / 8);
+		var endBits = 0;
+		var startBits = 0;
+		for (var i = 0; i < totalBytes; i++) {
+			if(totalBits > 8) {
+				endBits = endBit;
+				startBits = endBits + 7;
+				endBit = endBit + 8;
+				totalBits -= 8;
+			} else {
+				endBits = endBit;
+				startBits = endBits + totalBits - 1;
+				totalBits = 0;
+			}
+			var endChunk = chunk.length - Math.ceil((endBits + 1) / 8);
+			var startChunk = chunk.length - Math.ceil((startBits + 1) / 8);
+			var word = 0x0;
+			if (startChunk == endChunk){
+				var endOffset = endBits % 8;
+				var startOffset = startBits % 8;
+				var mask = 0xFF >> (8 - (startOffset - endOffset + 1));
+				word = (chunk[startChunk] >> endOffset) & mask;
+				array.unshift(word);
+			} else {
+				var endChunkEndOffset = endBits % 8;
+				var endChunkStartOffset = 7;
+				var endChunkMask = 0xFF >> (8 - (endChunkStartOffset - endChunkEndOffset + 1));
+				var endChunkWord = (chunk[endChunk] >> endChunkEndOffset) & endChunkMask;
+				var startChunkEndOffset = 0;
+				var startChunkStartOffset = startBits % 8;
+				var startChunkMask = 0xFF >> (8 - (startChunkStartOffset - startChunkEndOffset + 1));
+				var startChunkWord = (chunk[startChunk] >> startChunkEndOffset) & startChunkMask;
+				var startChunkWordShifted = startChunkWord << (endChunkStartOffset - endChunkEndOffset + 1);
+				word = endChunkWord | startChunkWordShifted;
+				array.unshift(word);
+			}
+		}
+		return array;
+	}
+
+	// Applies data type to a byte array
+	function apply_data_type(bytes, data_type) {
+		var output = 0;
+		if (data_type === "unsigned") {
+			for (var i = 0; i < bytes.length; ++i) {
+				output = (to_uint(output << 8)) | bytes[i];
+			}
+			return output;
+		}
+		if (data_type === "signed") {
+			for (var i = 0; i < bytes.length; ++i) {
+				output = (output << 8) | bytes[i];
+			}
+			// Convert to signed, based on value size
+			if (output > Math.pow(2, 8 * bytes.length - 1)) {
+				output -= Math.pow(2, 8 * bytes.length);
+			}
+			return output;
+		}
+		if (data_type === "bool") {
+			return !(bytes[0] === 0);
+		}
+		if (data_type === "hexstring") {
+			return toHexString(bytes);
+		}
+		return null; // Incorrect data type
+	}
+
+	// Decodes bitfield from the given chunk of bytes
+	function decode_field(chunk, size, start_bit, end_bit, data_type) {
+		var new_chunk = chunk.slice(0, size);
+		var chunk_size = new_chunk.length;
+		if (start_bit >= chunk_size * 8) {
+			return null; // Error: exceeding boundaries of the chunk
+		}
+		if (start_bit < end_bit) {
+			return null; // Error: invalid input
+		}
+		var array = extract_bytes(new_chunk, start_bit, end_bit);
+		return apply_data_type(array, data_type);
+	}
+
+	// Converts value to unsigned
+	function to_uint(x) {
+		return x >>> 0;
+	}
+
+	// Checks if two arrays are equal
+	function is_equal(arr1, arr2) {
+		if (arr1.length != arr2.length) {
+			return false;
+		}
+		for (var i = 0; i != arr1.length; i++) {
+			if (arr1[i] != arr2[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	// Converts array of bytes to hex string
+	function toHexString(byteArray) {
+		var arr = [];
+		for (var i = 0; i < byteArray.length; ++i) {
+			arr.push(('0' + (byteArray[i] & 0xFF).toString(16)).slice(-2));
+		}
+		return arr.join(' ');
+	}
+
+    // Converts array of bytes to 8 bit array
+    function convertToUint8Array(byteArray) {
+		var arr = [];
+		for (var i = 0; i < byteArray.length; i++) {
+			arr.push(to_uint(byteArray[i]) & 0xff);
+		}
+		return arr;
+	}
+    decoded_data["errors"] = errors;
+    return decoded_data;
